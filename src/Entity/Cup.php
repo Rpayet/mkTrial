1488c813cd302e927747d1a5269d2658719c0212
Rpayet <robinpayet@gmail.com>
2023-04-14 17:18:06 +0200

    Race Cup Relation

--- src/Entity/Cup.php
+++ src/Entity/Cup.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\CupRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: CupRepository::class)]
@@ -21,6 +23,14 @@ class Cup
 
     #[ORM\Column(length: 255)]
     private ?string $picture = null;
+
+    #[ORM\OneToMany(mappedBy: 'cup', targetEntity: Race::class)]
+    private Collection $races;
+
+    public function __construct()
+    {
+        $this->races = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -59,6 +69,36 @@ class Cup
     public function setPicture(string $picture): self
     {
         $this->picture = $picture;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Race>
+     */
+    public function getRaces(): Collection
+    {
+        return $this->races;
+    }
+
+    public function addRace(Race $race): self
+    {
+        if (!$this->races->contains($race)) {
+            $this->races->add($race);
+            $race->setCup($this);
+        }
+
+        return $this;
+    }
+
+    public function removeRace(Race $race): self
+    {
+        if ($this->races->removeElement($race)) {
+            // set the owning side to null (unless already changed)
+            if ($race->getCup() === $this) {
+                $race->setCup(null);
+            }
+        }
 
         return $this;
     }
