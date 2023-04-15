5ee2dae365f707c53a741f94d1c4b7ec95404d73
Rpayet <robinpayet@gmail.com>
2023-04-15 19:02:25 +0200

    EventForm - Paginate Cups

--- assets/react/controllers/Create-Comp/CupSelect.jsx
+++ assets/react/controllers/Create-Comp/CupSelect.jsx
@@ -1,26 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
+import ReactPaginate from "react-paginate";
 
-export default function CupSelect({ races, cupSelection, setCupSelection }) {
+export default function CupSelect({ races, cupSelection, setCupSelection, currentPage, setCurrentPage }) {
+
+    const cups = races.filter((race, index) => index % 4 === 0).map((race) => race.cup);
+    
+    const cupsPerPage = 12;
+    const pageCount = Math.ceil(cups.length / cupsPerPage);
+
+    const offset = currentPage * cupsPerPage;
+    let cupList = cups.slice(offset, offset + cupsPerPage);
+
+    // Fonction pour gérer le changement de page
+    const handlePageClick = (data) => {
+
+        const selectedPage = data.selected; 
+        const previousPage = currentPage - 1;
+        const nextPage = currentPage + 1;
+        
+        if (selectedPage === data.previousLabel) {
+        setCurrentPage(previousPage);
+        } else if (selectedPage === data.nextLabel) {
+        setCurrentPage(nextPage);
+        } else {
+        setCurrentPage(selectedPage);
+        }
+    }
 
     const handleSelect = (event) => {
         setCupSelection(parseInt(event.target.id));
     };
 
     return (
-
-        <div 
-            className="grid grid-cols-6 sm:gap-4">
-            {races.filter((race, index) => index % 4 === 0).map((race, index) => (
-                <img
-                id={race.cup.id}
-                className={ `w-38 p-1 sm:p-2 rounded-full cursor-pointer
-                            ${ cupSelection === race.cup.id ? 'bg-lumi' : '' }` }
-                key={ index }
-                src={ `/assets/admin/img/cups/${ race.cup.picture }` }
-                alt={`${ race.cup.name }`}
-                onClick={ handleSelect }
-                />
-            ))}
+        <div>
+            <div>
+                <ReactPaginate
+                        pageCount={pageCount}
+                        pageRangeDisplayed={1}
+                        marginPagesDisplayed={1}
+                        previousLabel="<"
+                        nextLabel=">"
+                        breakLabel="/"
+                        containerClassName="flex justify-center gap-4 mt-2"
+                        pageLinkClassName=""
+                        activeClassName="text-lumi border-solid border-b-2 border-lumi"
+                        onPageChange={handlePageClick}
+                        forcePage={currentPage}
+                    />
+            </div>
+            
+            <div 
+                className="grid grid-cols-6 sm:gap-4">
+                {cupList.map((cup, index) => (
+                    <img
+                    id={cup.id}
+                    className={ `w-38 p-1 sm:p-2 rounded-full cursor-pointer
+                                ${ cupSelection === cup.id ? 'bg-lumi' : '' }` }
+                    key={ index }
+                    src={ `/assets/admin/img/cups/${ cup.picture }` }
+                    alt={`${ cup.name }`}
+                    onClick={ handleSelect }
+                    />
+                ))}
+            </div>
         </div>
     )
 }
