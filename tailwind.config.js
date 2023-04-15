6530135aa929be46c9a1ca11017991954c1380bc
Rpayet <robinpayet@gmail.com>
2023-04-15 11:05:25 +0200

    Montserrat

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        'montserrat' : ['Montserrat', 'sans-serif'] 
+      },
       colors: {
         shell: '#015289'
       }
