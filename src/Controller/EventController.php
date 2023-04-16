a35778b403d188c472cc3f02474fc78ec9ae3986
Rpayet <robinpayet@gmail.com>
2023-04-16 14:23:55 +0200

    Event Page React Init

--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -2,17 +2,26 @@
 
 namespace App\Controller;
 
+use App\Entity\Tournament;
+use App\Repository\TournamentRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class EventController extends AbstractController
 {
-    #[Route('/event', name: 'app_event')]
-    public function index(): Response
+    #[Route('/event/{id}', name: 'app_event')]
+    public function index(TournamentRepository $repository, int $id): Response
     {
+        $event = $repository->find($id);
+
+        if (!$event) {
+            throw $this->createNotFoundException();
+        }
+
         return $this->render('event/index.html.twig', [
-            'controller_name' => 'EventController',
+            'event' => $event,
         ]);
     }
+
 }
