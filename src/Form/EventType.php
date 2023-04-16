ea1a40aec7c77521c8259bc538f4a5a845fa81ba
Rpayet <robinpayet@gmail.com>
2023-04-16 10:01:26 +0200

    FormType Setup

---
+++ src/Form/EventType.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Tournament;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class EventType extends AbstractType
+{
+
+
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('endAt', DateType::class, [
+                'widget' => 'single_text',
+                'format' => 'yyyy-MM-dd',
+            ])
+            ->add('race', null, [
+                'choice_label' => 'name',
+                'invalid_message' => 'Erreur de la sélection de course',
+            ])
+            ->add('speed')
+            ->add('privacy', null, [
+                'invalid_message' => 'Erreur',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Tournament::class,
+        ]);
+    }
+}
