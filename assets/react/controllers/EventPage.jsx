bde665c37f6a808593a60dbed91be06c1f09e7db
Rpayet <robinpayet@gmail.com>
2023-04-16 14:38:47 +0200

    Event Object Pass To react

--- assets/react/controllers/EventPage.jsx
+++ assets/react/controllers/EventPage.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 
-export default function EventPage() {
+export default function EventPage({ event }) {
 
+    console.log(event);
+    
     return (
         <div>
-            
+            <h1>Prout</h1>
+            <p>{`${event.name}`}</p>
         </div>
     )
 }
