51342dc0ec5474918026b5e1507cbd34d9f40caf
Rpayet <robinpayet@gmail.com>
2023-04-18 11:04:22 +0200

    fns test

--- assets/react/controllers/EventPage.jsx
+++ assets/react/controllers/EventPage.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import EventInfo from "./Event-Comp/EventInfo";
 import Userinfo from "./Event-Comp/UsersInfo";
+import { formatDistance } from "date-fns";
+import fr from "date-fns/locale/fr";
 
 export default function EventPage({ event }) {
 
     console.log(event);
+
+    const endAtDate = new Date(event.endAt.date);
 
     return (
         <div className="w-full flex flex-col items-center bg-white rounded-lg p-4">
@@ -18,7 +22,7 @@ export default function EventPage({ event }) {
                     <div 
                         id="event-options"
                         className="w-1/3" >
-                            <span>{`${new Date(event.endAt.date).toLocaleDateString()}`}</span>
+                            <span>{`${formatDistance(endAtDate, new Date(), { locale: fr, addSuffix: false })} restant`}</span>
 
                     </div>
 
