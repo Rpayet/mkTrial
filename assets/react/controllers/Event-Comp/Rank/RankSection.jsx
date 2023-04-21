3f5df3266af64abedabe8ccd5984840ab0539367
Rpayet <robinpayet@gmail.com>
2023-04-21 16:51:59 +0200

    Rank Section format

--- assets/react/controllers/Event-Comp/Rank/RankSection.jsx
+++ assets/react/controllers/Event-Comp/Rank/RankSection.jsx
@@ -1,41 +1,19 @@
 import React from "react";
+import Ranking from "./Ranking";
+import AddEntry from "./AddEntry";
 
-export default function RankSection({ event }) {
+export default function RankSection({ event, entries, isUserRegistered }) {
 
     return (
         <div 
             id="rank-container"
             className="w-2/3 flex flex-col items-center">
-
                 
-                { Array.from({length: 1}).map((_, i) => (
-                <div className="w-full mt-2 py-4 px-10 bg-white flex justify-between rounded-lg">
-                    <div className="flex w-1/3 gap-2">
-                        <span>#01</span>
-                        <p>{event.user.name}</p>
-                    </div>
-                    <div>
-                        <span className="w-1/3">1:22.333</span>
-                    </div>
-                </div> 
-                )) }
-                <div 
-                    className="w-full mt-2 py-4 px-10 bg-white flex 
-                    justify-center rounded-lg cursor-pointer
-                    hover:border-solid hover:border-[1px] hover:border-lumi">
-                    <div className="flex gap-2">
-                        
-                        <img 
-                            src="/assets/admin/img/icons/Add.svg" 
-                            alt="add"
-                            className="w-6 border-solid border-[1px] border-silver rounded-full"
-                                />
-                        <p>Ajouter une entrée</p>
-                        
-                    </div>
-                    
-                </div> 
+                <Ranking 
+                    event= { event }
+                    entries= { entries } />
 
+                { isUserRegistered && <AddEntry /> }
         </div>
 
     )
